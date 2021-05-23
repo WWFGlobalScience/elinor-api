def98cd8c72e928c833246b8b13104a1b909782b
Kim Fisher <kfisher@wcs.org>
2021-05-22 21:58:27 -0400

    configure public S3 image links

--- src/app/settings.py
+++ src/app/settings.py
@@ -162,6 +162,7 @@ AWS_REGION = AWS_S3_REGION_NAME = os.environ.get("AWS_REGION")
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_LOCATION = f"{ENVIRONMENT}/"
+AWS_QUERYSTRING_AUTH = False
 
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
