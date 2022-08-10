78576542dd13088fea68ef6a79ed407939e2c522
Kim Fisher <kfisher@wcs.org>
2022-08-10 18:34:05 -0400

    contact assessment admins basic support

--- src/api/resources/contact.py
+++ src/api/resources/contact.py
@@ -8,15 +8,49 @@ from rest_framework import serializers
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from ..utils.email import email_elinor_admins
+from ..models import Assessment
+from ..utils.email import email_assessment_admins, email_elinor_admins
 
 
-class ContactElinorAdminsSerializer(serializers.Serializer):
-    recaptcha = ReCaptchaV3Field(action="contact_elinor_admins")
+class ContactSerializer(serializers.Serializer):
     name = serializers.CharField()
     email = serializers.EmailField()
     subject = serializers.CharField()
     message = serializers.CharField()
+
+
+class ContactElinorAdminsSerializer(ContactSerializer):
+    recaptcha = ReCaptchaV3Field(action="contact_elinor_admins")
+
+
+class ContactAssessmentAdminsSerializer(ContactSerializer):
+    recaptcha = ReCaptchaV3Field(action="contact_assessment_admins")
+    assessment = serializers.IntegerField()
+
+
+def _process_contact_request(serializer, email_function):
+    serializer.is_valid(raise_exception=True)
+    assessment_id = serializer.validated_data.get("assessment")
+    assessment = None
+    if assessment_id:
+        try:
+            assessment = Assessment.objects.get(pk=assessment_id)
+        except Assessment.DoesNotExist:
+            return Response(
+                f"assessment {assessment_id} does not exist",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    kwargs = {
+        "name": serializer.validated_data.get("name"),
+        "from_email": serializer.validated_data.get("email"),
+        "subject": serializer.validated_data.get("subject"),
+        "message": serializer.validated_data.get("message"),
+        "assessment": assessment,
+    }
+    email_function(**kwargs)
+
+    return Response(status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
@@ -26,12 +60,14 @@ def contact_elinor_admins(request):
     serializer = ContactElinorAdminsSerializer(
         data=request.data, context={"request": request}
     )
-    serializer.is_valid(raise_exception=True)
+    return _process_contact_request(serializer, email_elinor_admins)
 
-    name = serializer.validated_data.get("name")
-    email = serializer.validated_data.get("email")
-    subject = serializer.validated_data.get("subject")
-    message = serializer.validated_data.get("message")
-    email_elinor_admins(subject, message, name, email)
 
-    return Response(status=status.HTTP_200_OK)
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes((AllowAny,))
+def contact_assessment_admins(request):
+    serializer = ContactAssessmentAdminsSerializer(
+        data=request.data, context={"request": request}
+    )
+    return _process_contact_request(serializer, email_assessment_admins)
